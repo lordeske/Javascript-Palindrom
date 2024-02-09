f76316ea111702008fa3b7ddfb30d26399ee1af5
unknown <eskicmihajlo55@gmail.com>
2024-02-09 22:09:23 +0100

    dodato osnove

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const word = document.getElementById("text-input");
+const button = document.getElementById("check-btn");
+const log = document.getElementById("result")
+
+button.addEventListener("click", ()=>{
+
+    log.innerText="";
+
+    if(word.value === "")
+    {
+        alert("Please input a value");
+        
+    }
+    else
+    {
+        let first = word.value
+        let last = first.split("").reverse().join("");
+        console.log(last)
+    }
+
+
+
+
+
+
+
+
+})
+
+
 
